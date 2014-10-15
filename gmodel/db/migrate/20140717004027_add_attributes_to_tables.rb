e470eeff3a4b2acd064495ec946bf9c6ecdfc83d
Charle Walker <charliehwalker@gmail.com>
2014-10-15 11:37:00 -0400

    remove pad repository reorganize

--- gmodel/db/migrate/20140717004027_add_attributes_to_tables.rb
+++
@@ -1,8 +0,0 @@
-class AddAttributesToTables < ActiveRecord::Migration
-  def change
-  	#add_column :table_name, :attribute_name, :data_type
-  	add_column :users, :username, :text
-  	add_column :tweets, :content, :text
-  	add_column :tweets, :user_id, :integer
-  end
-end
